197c2d7ef27a1f74385a68a6c8f601bf9d19302f
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-01 10:05:05 +0300

    completed adding solutions to index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(x, y) {
+    return x+y;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(x, y) {
+    return x*y;
+}
+
+function divide(x, y) {
+    return x/y;
+}
+
+function substract(a, b) {
+    return b-a;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function increment(n) {
+    return n+1;
+}
+
+function decrement(n) {
+    return n-1;
+}
+
+function makeInt(n) {
+    return  parseInt (n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
